900b6135d61eb9f793164630b577a772486675f5
egfedo <fegor04@gmail.com>
2024-11-02 12:08:59 +0300

    move tetris to its own folder

--- tetris/main.js
+++ tetris/main.js
@@ -3,7 +3,7 @@ var nameField = document.getElementById("name");
 function click() {
     if (nameField.value.length != 0) {
         localStorage["tetris.username"] = nameField.value;
-        window.location.href = "./game.html";
+        window.location.href = "game.html";
     }
 }
 nameField.value = (_a = localStorage["tetris.username"]) !== null && _a !== void 0 ? _a : "";
